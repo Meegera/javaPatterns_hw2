4017131caca1cfb62c8f416b4da80f581cc8fd36
Greta Donskova <gretadonskova@gmail.com>
2023-10-12 21:45:06 +0300

    Initial commit

---
+++ task2/src/main/java/org/structural_patterns_task2/BinOps.java
@@ -0,0 +1,13 @@
+package org.structural_patterns_task2;
+
+public class BinOps {
+    public String sum(String a, String b) {
+        int result = Integer.parseInt(a, 2) + Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+
+    public String mult(String a, String b) {
+        int result = Integer.parseInt(a, 2) * Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+}
